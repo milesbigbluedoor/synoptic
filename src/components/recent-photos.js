38691cd24c6de28a30f1fe87768c920618dfd4e6
Miles Lemon <miles@bigbluedoor.net>
2019-11-05 14:39:38 +0000

    create recent photos component

---
+++ src/components/recent-photos.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import LinkButton from "./link-button"
+
+const RecentPhotos = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allNodePhoto(limit: 3, sort: { fields: created, order: ASC }) {
+        edges {
+          node {
+            id
+            title
+            relationships {
+              field_photograph {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 600, quality: 100) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="recent-photos">
+      <h2 className="recent-photos__title">Recent Photos</h2>
+      <div className="recent-photos__wrapper">
+        {data.allNodePhoto.edges.map(edge => (
+          <div className="recent-photos__card" key={edge.node.id}>
+            <Img
+              fluid={
+                edge.node.relationships.field_photograph.localFile
+                  .childImageSharp.fluid
+              }
+            />
+            <div className="recent-photos__card-title">
+              <h3>{edge.node.title}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+      <LinkButton url="/photos" text="View all" />
+    </div>
+  )
+}
+
+export default RecentPhotos
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Hero from "../components/hero"
+import RecentPhotos from "../components/recent-photos"
 
 const Index = () => (
   <Layout>
     <SEO title="Home | John Doe Photography" />
     <Hero />
+    <RecentPhotos />
   </Layout>
 )
 
