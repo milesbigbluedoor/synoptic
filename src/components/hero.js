334a4a9164367f5593efd3cd87804fe628e681f7
Miles Lemon <miles@bigbluedoor.net>
2019-11-05 13:40:28 +0000

    create and style hero component

---
+++ src/components/hero.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      heroImage: file(relativePath: { eq: "mountains.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="hero">
+      <Img
+        fluid={data.heroImage.childImageSharp.fluid}
+        style={{ maxHeight: "700px" }}
+      />
+      <div className="hero__text">
+        <div className="hero__text-wrapper">
+          <h2 className="hero__title">John Doe Photography</h2>
+          <p className="hero__description">
+            A freelance photographer based out of South-East England.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
