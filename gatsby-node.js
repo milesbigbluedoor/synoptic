7f7f7bc45f20422c202e38892f065f12d7de153a
Miles Lemon <miles@bigbluedoor.net>
2019-11-05 09:30:04 +0000

    setup linting and SCSS

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,29 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allNodeBlog {
+        edges {
+          node {
+            id
+            path {
+              alias
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allNodeBlog.edges.forEach(({ node }) => {
+      createPage({
+        path: node.path.alias,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+}
